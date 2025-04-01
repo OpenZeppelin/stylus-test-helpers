7d2aabaa21ed11ab7fe40a4ef9eb1a967796c094
0xNeshi <nenad.misic@openzeppelin.com>
2025-04-01 12:00:37 +0200

    fix: stop dropping precompiles

--- crates/motsu/src/precompiles/mod.rs
+++ crates/motsu/src/precompiles/mod.rs
@@ -1,10 +1,12 @@
 //! Ethereum Precompiles
+use stylus_sdk::storage::StorageType;
+
 use crate::context::Contract;
 
 pub(crate) mod ecrecover;
 
 /// Deploy precompiled contracts.
 /// See: <https://ethereum.github.io/yellowpaper/paper.pdf>
-pub fn deploy_precompiles() {
-    _ = Contract::<ecrecover::EcRecover>::new_at(ecrecover::ADDRESS);
+pub fn deploy_precompiles() -> Vec<Contract<impl StorageType>> {
+    vec![Contract::<ecrecover::EcRecover>::new_at(ecrecover::ADDRESS)]
 }
